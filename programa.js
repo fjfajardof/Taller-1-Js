106ad7a0c56cb888890ac8349c3cfd9f168f8ee7
Fabian Fajardo <fabianjff97@gmail.com>
2023-03-05 14:01:01 -0500

    V1

---
+++ programa.js
@@ -0,0 +1,28 @@
+console.log("Taller 1.1");
+let a=34;
+let b=56;
+if(a<b){
+    console.log( "El número menor entre "+ a + " y " + b + " es "+ a);
+} else{
+    console.log("El número menor entre "+ a + " y " + b + " es "+ b);
+}
+console.log("    ");
+
+
+console.log("Taller 1.2");
+let x=24;
+let y=60;
+let z=150;
+if(x>y && x>z){
+    console.log("El número mayor entre "+ x + ", " + y + " y "+ z + " es "+ x  );
+} else{
+    if(y>z){
+        console.log("El número mayor entre "+ x + ", " + y + " y "+ z + " es "+ y  );
+    }else{
+        console.log("El número mayor entre "+ x + ", " + y + " y "+ z + " es "+ z  );
+    }
+
+}
+
+
+
